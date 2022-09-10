c057165fe33ddadedeedd9c7cac0658f556a30f3
MehlMoon <arandamendozaelsa@gmail.com>
2022-09-10 12:11:35 -0500

    Buenos días alegria, buenos días señor sol :D

---
+++ src/temas-ts/01-tipos-datos.ts
@@ -0,0 +1,14 @@
+console.log("Hola Mundo, soy Elsa :D");
+
+var t1:number | string =3; // Mejor no usarla
+let t2:number | string=2; // Puede cambiar
+let activo:boolean=false;// El tipado va con dos puntos seguido del nombre
+const t3=666; // Ya no puede cambiar, es constante 
+
+t1='www';
+
+t2='www';
+
+console.log(t1)
+console.log(t2)
+
